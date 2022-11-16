7887a65b5bf0f59c6c72b1d2a24c934bde28aa83
Miguel Nazario <mnazario35@gmail.com>
2022-11-16 18:01:08 -0500

    Installed MUI Icons and set up navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App/App';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#210f08',
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
