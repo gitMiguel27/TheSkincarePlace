a9c76f07f1a19a15b36af6d25d93eb3f1a3aa68b
Miguel Nazario <mnazario35@gmail.com>
2022-11-30 15:14:25 -0500

    Done loading products and styling skin type page

--- client/src/Components/SkinType/SkinType.js
+++ client/src/Components/SkinType/SkinType.js
@@ -16,56 +16,81 @@ function SkinType({ skintypes }) {
 
     return (
         <div className='skin-type'>
-            <Grid2 container rowSpacing={2} columnSpacing={0} display="flex" justifyContent="center" alignItems="center" className="grid-container">
-                <Grid2 container spacing={4} xs={8} display="flex" justifyContent="center" alignItems="center" pl={15}>
-                    <Grid2 item xs={12}>
-                        <Typography>About Your {skintype.name} Skin:</Typography>
-                    </Grid2>
-                    <Grid2 item xs={12}>
-                        <Typography>{skintype.body}</Typography>
-                    </Grid2>
-                </Grid2>
-                <Grid2 container md={4} className='skintype-image-container'>
-                    <Grid2 item xs={12} display="flex" justifyContent="center" alignItems="center">
-                        <Card sx={{ maxWidth: 345 }}>
-                            <CardMedia
-                                component="img"
-                                height="194"
-                                image={skintype.image}
-                                alt={skintype.name}
-                            />
-                        </Card>
-                    </Grid2>
-                </Grid2>
-            </Grid2>
-            <Grid2 container rowSpacing={2} columnSpacing={2} className='skintype-image-container' mb={15}>
-                {products.map(product => {
-                    return (
-                        <Grid2 container md={3} mb={5} key={product.name}>
-                            <Grid2 item xs={8}>
-                                <Paper elevation={5} sx={{ backgroundColor: (theme) => "primary.main" }}>
-                                    <Grid2 container xs={12} className='product-card-container'>
-                                        <Grid2 item xs={8} display="flex" justifyContent="center" alignItems="center">
-                                            <Card sx={{ maxWidth: 345 }}>
-                                                <CardMedia
-                                                    component="img"
-                                                    height="194"
-                                                    image={product.image}
-                                                    alt={product.name}
-                                                />
-                                            </Card>
-                                        </Grid2>
-                                        <Grid2 item xs={4}>
-                                            <Typography>
-                                                {product.concern}
-                                            </Typography>
-                                        </Grid2>
-                                    </Grid2>
-                                </Paper>
-                            </Grid2>
+            <Grid2 container rowSpacing={2} columnSpacing={2} display="flex" justifyContent="center" alignItems="center" >
+                <Grid2 container columnSpacing={4} display="flex" justifyContent="center" alignItems="center" className="grid-container">
+                    <Grid2 container spacing={4} xs={8} display="flex" justifyContent="center" alignItems="center" pl={15}>
+                        <Grid2 item xs={12}>
+                            <Typography>About Your {skintype.name} Skin:</Typography>
                         </Grid2>
-                    )
-                })}
+                        <Grid2 item xs={12}>
+                            <Typography>{skintype.body}</Typography>
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 container md={4} className='skintype-image-container'>
+                        <Grid2 item xs={12} display="flex" justifyContent="center" alignItems="center">
+                            <Card sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    component="img"
+                                    height="194"
+                                    image={skintype.image}
+                                    alt={skintype.name}
+                                />
+                            </Card>
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
+                <Grid2 container xs={8} md={12} mt={8} mb={8} display="flex" justifyContent="center" alignItems="center" >
+                    {products.map(product => {
+                        return (
+                            <Grid2 container md={3} mb={3} key={product.name}>
+                                <Grid2 item xs={12}>
+                                    <Paper elevation={5} sx={{ backgroundColor: (theme) => "primary.main" }}>
+                                        <Grid2 container xs={12} className='product-card-container'>
+                                            <Grid2 item xs={6} display="flex" justifyContent="left" alignItems="center">
+                                                <Card sx={{ width: 345 }}>
+                                                    <CardMedia
+                                                        component="img"
+                                                        height="200"
+                                                        image={product.image}
+                                                        alt={product.name}
+                                                    />
+                                                </Card>
+                                            </Grid2>
+                                            <Grid2 container xs={6} display="flex" justifyContent="center" alignItems="center">
+                                                <Grid2 item xs={12}>
+                                                    <Typography textAlign="center" sx={{ color: "white" }}>
+                                                        {product.concern}
+                                                    </Typography>
+                                                </Grid2>
+                                                <Grid2 item xs={12}>
+                                                    <Typography textAlign="center" sx={{ color: "white" }}>
+                                                        {product.company}
+                                                        <br/>
+                                                        {product.name}
+                                                        <br/>
+                                                        ${product.price}
+                                                    </Typography>
+                                                </Grid2>
+
+                                                {/* <Typography textAlign="center" sx={{ color: "white" }}>
+                                                    {product.concern}
+                                                    <br/>
+                                                    <br/>
+                                                    <br/>
+                                                    {product.company}
+                                                    <br/>
+                                                    {product.name}
+                                                    <br/>
+                                                    ${product.price}
+                                                </Typography> */}
+                                            </Grid2>
+                                        </Grid2>
+                                    </Paper>
+                                </Grid2>
+                            </Grid2>
+                        )
+                    })}
+                </Grid2>
             </Grid2>
         </div>
     );
